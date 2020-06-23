143d1f5b2692aa591f39446085f13f33f5d4ff65
atajmouti <atajmouti@gmail.com>
2020-06-23 13:07:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# demo-gtmt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/demo-gtmt)
---
+++ src/app/demo-gtmt.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+
+/**
+ * @title Stepper that displays errors in the steps
+ */
+@Component({
+  selector: 'demo-gtmt',
+  templateUrl: 'demo-gtmt.html',
+  styleUrls: ['demo-gtmt.scss'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
+  }]
+})
+export class DemoGTMT implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
